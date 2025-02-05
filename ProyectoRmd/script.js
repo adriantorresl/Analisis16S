ecc2863787bf8603fc749fd6a2d49d0d1cdfb037
Adrian Torres <92758447+adriantorresl@users.noreply.github.com>
2025-02-04 19:00:07 -0600

    Delete ProyectoRmd directory

--- ProyectoRmd/script.js
+++
@@ -1,5 +0,0 @@
-// Puedes agregar interactividad aquí si lo deseas
-// Por ejemplo, animaciones o efectos adicionales
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("La página está lista!");
-  });
