f84e7f854c950d32dfbf04880514908bdc9bc90a
Adrian Torres <92758447+adriantorresl@users.noreply.github.com>
2025-02-04 18:59:47 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Puedes agregar interactividad aquí si lo deseas
+// Por ejemplo, animaciones o efectos adicionales
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("La página está lista!");
+  });
